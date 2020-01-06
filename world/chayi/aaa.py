7f91398cac896de706a9e871995801b8ef87c347
cheyennellin <44136411+cheyennellin@users.noreply.github.com>
2020-01-06 14:51:14 +0800

    重新上传代码

--- world/chayi/aaa.py
+++ world/chayi/aaa.py
@@ -23,6 +23,8 @@ app = Flask(__name__)
 # pandas 大法读内容, 用dropna()丢缺失值, 用unique()取唯一值, 不重覆
 df = pd.read_csv('gender.csv', encoding='utf-8')
 regions_available_loaded = list(df.region.dropna().unique())
+df1 = pd.read_csv('sha.csv', encoding='utf-8')
+regions_available_loaded = list(df.region.dropna().unique())
 # 基本cufflinks 及ploty設置, 查文檔看書貼上而已
 cf.set_config_file(offline=True, theme="ggplot")
 py.offline.init_notebook_mode()
@@ -43,6 +45,7 @@ def hu_run_2019():
     return render_template('results2.html',
                            the_res = data_str,   # 表
                            the_select_region=regions_available)   
+
 
 @app.route('/hurun',methods=['POST'])
 def hu_run_select() -> 'html':
@@ -84,5 +87,42 @@ def hu_run_select() -> 'html':
                             the_res = data_str,
                             the_select_region=regions_available,
                            )
+
+
+@app.route('/kill',methods=['GET','POST'])
+def kill() -> 'html':
+    data_str = df1.to_html()
+    trace0 = go.Bar(
+        y=df1['2010'],
+        x=df1['indictor'],
+        marker=dict(color='#483D8B',  # 设置条形图的颜色
+                    line=dict(color='rgb(256, 256, 256)', width=1.0, )),  # 设置条形图边框
+        name='总次数',  # 设置这个图的名字，和图例对应#如果水平条形图需设置，竖直条形图不用设置
+        opacity=0.9)
+    layout = go.Layout(
+        plot_bgcolor='#E6E6FA',  # 图的背景颜色
+        paper_bgcolor='#F8F8FF',  # 图像的背景颜色
+        autosize=False, width=1050, height=600,  # 设置图像的大小
+        # 设置图离图像四周的边距
+        margin=go.Margin(l=250, r=60, b=50, t=60, pad=3),  # pad参数是刻度与标签的距离
+        # 设置y轴的刻度和标签
+        yaxis=dict(title='遭到凶杀的女性人数per1,000,000人',  # 设置坐标轴的标签
+                   titlefont=dict(color='rgb(148, 103, 189)', size=30),  # 设置坐标轴标签的字体及颜色
+                   tickfont=dict(color='rgb(148, 103, 189)', size=20),  # 设置刻度的字体大小及颜色
+                   showticklabels=True,  # 设置是否显示刻度
+                   autorange=False,
+                   range=[0, 3000],
+                   type='linear'
+                   ))
+    fig = go.Figure(layout=layout, data=trace0)
+    py.offline.plot(fig, filename="sha.html", auto_open=False)
+    with open("sha.html", encoding="utf8", mode="r") as f:  # 把"成果.html"當文字檔讀入成字符串
+        plot_all = "".join(f.readlines())
+    title = ('女性受暴力程度')
+    return render_template('kill.html',
+                           the_title = title,
+                           the_plot_all = plot_all,
+                           )
+
 if __name__ == '__main__':
     app.run(debug=True,port=8002)						 
